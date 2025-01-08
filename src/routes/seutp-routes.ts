7bc29b4f20ff2d5cf06453236b023c6581d8d1c4
Danilo Romão <htpps.sw@gmail.com>
2025-01-07 23:42:16 -0300

    styles: change file names

--- src/routes/seutp-routes.ts
+++ src/routes/seutp-routes.ts
@@ -1,9 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { CreateBook } from "./book/create";
 import { FindAllBooks } from "./book/find-all";
-import { FindBookByCategory } from "./book/find-by-category";
 import { FindBookBySlug } from "./book/find-by-slug";
-import { FindBookByAuthor } from "./book/find-by-author";
 
 
 export async function RegisterRoutes(server: FastifyInstance) {
@@ -16,6 +14,4 @@ export async function RegisterRoutes(server: FastifyInstance) {
 	server.register(CreateBook)
 	server.register(FindBookBySlug)
 	server.register(FindAllBooks)
-	server.register(FindBookByCategory)
-	server.register(FindBookByAuthor)
 }
