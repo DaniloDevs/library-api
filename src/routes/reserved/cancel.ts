ab105666577237b26648fe054b5297d3b491d574
Danilo Romão <htpps.sw@gmail.com>
2025-01-13 01:21:22 -0300

    Feature: criar rota e testes para cancelar uma reserva

---
+++ src/routes/reserved/cancel.ts
@@ -0,0 +1,44 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "../../lib/prisma";
+
+// adicionar uma data quando foi cancelado
+export async function CancelReservertion(server: FastifyInstance) {
+     server
+          .withTypeProvider<ZodTypeProvider>()
+          .delete('/reservations/:reservationId', {
+               schema: {
+                    params: z.object({
+                         reservationId: z.string()
+                    })
+               }
+          }, async (request, reply) => {
+               const { reservationId } = request.params
+
+               const existReserverd = await prisma.reservations.findUnique({ where: { id: reservationId } })
+
+               if (!existReserverd) return reply.status(400).send({ Message: "O ID informado não refere a nenhuma reservar do sistema" })
+
+               if (existReserverd.status === "CANCELLED") return reply.status(400).send({
+                    Message: "A reserva do ID informado já foi cancelada"
+               })
+
+               await prisma.reservations.update({
+                    where: { id: reservationId },
+                    data: {
+                         status: "CANCELLED",
+                         Books: {
+                              update: {
+                                   status: "AVAILABLE"
+                              }
+                         }
+                    }
+               })
+
+               return reply.status(200).send({
+                    Message: "O cancelamento da reservar foi feito com sucesso",
+                    ReservationId: existReserverd.id
+               })
+          })
+}
---
+++ test/reserved/cancel.spec.ts
@@ -0,0 +1,62 @@
+import { afterAll, describe, expect, test } from 'vitest'
+import { prisma } from '../../src/lib/prisma'
+import server from '../../src/server'
+
+
+describe('Cancel Register Routes', async () => {
+     afterAll(async () => {
+          await prisma.reservations.update({
+               where: { id: reservationId },
+               data: {
+                    status: 'ACTIVE',
+                    Books: {
+                         update: {
+                              status: 'RESERVED'
+                         }
+                    }
+               }
+          })
+     })
+
+     const res = await server.inject({
+          method: 'GET',
+          url: '/reservations',
+     })
+
+     const { Reservations } = JSON.parse(res.body)
+
+     const reservationId = Reservations[0].id
+
+     test('Deve ser possivel poder cancelar a reservar de um livro', async () => {
+          const response = await server.inject({
+               method: 'DELETE',
+               url: `/reservations/${reservationId}`,
+          })
+          const { Message } = JSON.parse(response.body)
+
+          expect(Message).toBe("O cancelamento da reservar foi feito com sucesso")
+          expect(response.statusCode).toBe(200)
+     })
+
+     test('Não deve ser possivel poder cancelar uma reserva já cancelada', async () => {
+          const response = await server.inject({
+               method: 'DELETE',
+               url: `/reservations/${reservationId}`,
+          })
+          const { Message } = JSON.parse(response.body)
+
+          expect(Message).toBe("A reserva do ID informado já foi cancelada")
+          expect(response.statusCode).toBe(400)
+     })
+
+     test('Não deve ser possivel poder cancelar uma reserva que não existe', async () => {
+          const response = await server.inject({
+               method: 'DELETE',
+               url: `/reservations/36780413-3410413`,
+          })
+          const { Message } = JSON.parse(response.body)
+
+          expect(Message).toBe("O ID informado não refere a nenhuma reservar do sistema")
+          expect(response.statusCode).toBe(400)
+     })
+})
